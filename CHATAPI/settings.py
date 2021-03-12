2e9f1442674c9ef5d7cacc5abe0f7097004248b4
rohit1717 <rohitsingh17172000@gmail.com>
2021-03-12 19:51:00 +0530

    qw

--- CHATAPI/settings.py
+++ CHATAPI/settings.py
@@ -160,18 +160,19 @@ AWS_HOST_REGION=config('AWS_HOST_REGION')
 AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL=None
 
-AWS_LOCATION = config('AWS_LOCATION')
+AWS_LOCATION = 'static'
 MEDIA_URL='/media/'
 
 
 STATIC_URL='https://%s/%s/'% (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
 
-STATICFILES_STORAGE = config('STATICFILES_STORAGE')
 
 AWS_S3_OBJECT_PARAMETERS={
     'CacheControl':'max-age=86400',
 
 }
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-DEFAULT_FILE_STORAGE='CHATAPI.storage_backends.MediaStorage'
+
 SOCKET_SERVER=config("SOCKET_SERVER")
